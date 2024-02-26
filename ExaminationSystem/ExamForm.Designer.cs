1e9df387af8679332c18377b3674884e5a58554a
ARahmaan96 <a.rahman.khallaf@gmail.com>
2024-02-27 00:48:35 +0200

    Adding Exam Form.

---
+++ ExaminationSystem/ExamForm.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace ExaminationSystem
+{
+    partial class ExamForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label5 = new Label();
+            label4 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            button1 = new Button();
+            panel2 = new Panel();
+            radioButton4 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label1 = new Label();
+            panel3 = new Panel();
+            button3 = new Button();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            label3 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.BackColor = Color.FromArgb(49, 123, 209);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(0, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1015, 37);
+            panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(833, 6);
+            label5.Name = "label5";
+            label5.Size = new Size(52, 25);
+            label5.TabIndex = 2;
+            label5.Text = "Year:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(398, 6);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 25);
+            label4.TabIndex = 1;
+            label4.Text = "Proggress:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(3, 6);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 25);
+            label2.TabIndex = 0;
+            label2.Text = "Subject: ";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(633, 378);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 42);
+            button2.TabIndex = 1;
+            button2.Text = "Next";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(552, 379);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 41);
+            button1.TabIndex = 0;
+            button1.Text = "Previous";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.BackColor = Color.WhiteSmoke;
+            panel2.Controls.Add(radioButton4);
+            panel2.Controls.Add(radioButton3);
+            panel2.Controls.Add(radioButton2);
+            panel2.Controls.Add(radioButton1);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(12, 81);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(696, 279);
+            panel2.TabIndex = 2;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(21, 200);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(94, 19);
+            radioButton4.TabIndex = 4;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "radioButton4";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(21, 158);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(94, 19);
+            radioButton3.TabIndex = 3;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(21, 110);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 19);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(21, 64);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(94, 19);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // panel3
+            // 
+            panel3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            panel3.AutoScroll = true;
+            panel3.Location = new Point(714, 81);
+            panel3.MaximumSize = new Size(286, 279);
+            panel3.MinimumSize = new Size(286, 279);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(286, 279);
+            panel3.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button3.Location = new Point(896, 44);
+            button3.Name = "button3";
+            button3.Size = new Size(104, 31);
+            button3.TabIndex = 4;
+            button3.Text = "Submit";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.WhiteSmoke;
+            panel4.Controls.Add(panel5);
+            panel4.Location = new Point(5, 473);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(982, 19);
+            panel4.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(255, 192, 128);
+            panel5.Location = new Point(1, 1);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(895, 19);
+            panel5.TabIndex = 0;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(6, 47);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 21);
+            label3.TabIndex = 6;
+            label3.Text = "Name: ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(494, 6);
+            label6.Name = "label6";
+            label6.Size = new Size(59, 25);
+            label6.TabIndex = 3;
+            label6.Text = "15/20";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(886, 7);
+            label7.Name = "label7";
+            label7.Size = new Size(99, 25);
+            label7.TabIndex = 4;
+            label7.Text = "2023/2024";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(79, 6);
+            label8.Name = "label8";
+            label8.Size = new Size(34, 25);
+            label8.TabIndex = 5;
+            label8.Text = "CS";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(59, 49);
+            label9.Name = "label9";
+            label9.Size = new Size(135, 21);
+            label9.TabIndex = 7;
+            label9.Text = "Mohamed Ahmed";
+            // 
+            // ExamForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1012, 523);
+            Controls.Add(label9);
+            Controls.Add(label3);
+            Controls.Add(panel4);
+            Controls.Add(button3);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(panel1);
+            Name = "ExamForm";
+            Text = "ExamForm";
+            Load += ExamForm_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel4.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button2;
+        private Button button1;
+        private Panel panel2;
+        private Panel panel3;
+        private Button button3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label1;
+        private Panel panel4;
+        private Panel panel5;
+        private Label label5;
+        private Label label4;
+        private Label label2;
+        private Label label3;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label9;
+    }
+}
